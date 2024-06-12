7fe2458470c3efdb187fe3af8ae139aabb3050a7
ALDono <aubreyldono@gmail.com>
2024-06-12 14:40:32 +0100

    Navigation complete

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const btnHamburger = document.getElementById("btnHamburger");
-    const navigationLinks = document.querySelector("#shopLinksNav")
-    let navActive = false;
-    btnHamburger.addEventListener("click", function(){
-      if(navActive === false){
-        navActive = true;
-        navigationLinks.style.display = "flex";
-      }else{
-        navActive = false;
-        navigationLinks.style.display = "none";
-      }
-    })
+const btnHamburger = document.querySelector("#btnHamburger");
+const navigationLinks = document.querySelector("#shopLinksNav");
+const darken = document.querySelector("#darken");
+let navActive = false;
+btnHamburger.addEventListener("click", function(){
+  if(navActive === false){
+    navActive = true;
+    navigationLinks.style.display = "flex";
+    darken.style.display = "block";
+  }else{
+    navActive = false;
+    navigationLinks.style.display = "none";
+    darken.style.display = "none";
+  }
+})
+
+darken.addEventListener("click",function(){
+  navActive = false;
+  navigationLinks.style.display = "none";
+  darken.style.display = "none";
+})
